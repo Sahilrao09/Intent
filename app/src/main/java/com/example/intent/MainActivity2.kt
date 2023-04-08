0532b192f237dcf9e449cc978fa209469cc65c4e
Sahil Rao <88286056+Sahilrao09@users.noreply.github.com>
2023-04-08 12:31:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/intent/MainActivity2.kt
@@ -0,0 +1,17 @@
+package com.example.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val button2:Button = findViewById(R.id.button2)
+        button2.setOnClickListener {
+            finish()
+        }
+    }
+}
